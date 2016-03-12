f144ebe37befdd4a0f40300453b3973776201521
Ben Crastnopol <bcrastnopol@shoobx.com>
2016-03-11 23:29:52 -0500

    it draws and zooms!
    canvas zooms
    
    deep in rotation
    
    saving my workm trying a new approach to rotation
    
    add fabricState

--- web/pdf_page_view.js
+++ web/pdf_page_view.js
@@ -85,6 +85,15 @@ class PDFPageView {
     this.textLayer = null;
     this.zoomLayer = null;
 
+    this.fabricState = {
+      /* We keep track of canvas scale separately because we load the canvas background into
+       * a fabric class after it has been scaled and we need to know how to scale the other objects as well.
+       */
+      canvasScale: this.scale,
+      objs: null,
+      rotation: this.rotation,
+    };
+
     let div = document.createElement('div');
     div.className = 'page';
     div.style.width = Math.floor(this.viewport.width) + 'px';
