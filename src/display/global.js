05793585745bf5992f78fe44cdb4da8a03a1692a
Ben Crastnopol <bcrastnopol@shoobx.com>
2017-04-16 01:25:25 -0400

    move draw to web folder

--- src/display/global.js
+++ src/display/global.js
@@ -1,4 +1,4 @@
-/* Copyright 2015 Mozilla Foundation
+ /* Copyright 2015 Mozilla Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -105,7 +105,6 @@ PDFJS.maxImageSize = (PDFJS.maxImageSize === undefined ?
  * @var {string}
  */
 PDFJS.cMapUrl = (PDFJS.cMapUrl === undefined ? null : PDFJS.cMapUrl);
-
 /**
  * Specifies if CMaps are binary packed.
  * @var {boolean}
